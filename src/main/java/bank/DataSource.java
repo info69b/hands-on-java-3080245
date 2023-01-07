607cb42e0042500a31d9494eed88150a86a8b301
info69b <42078775+info69b@users.noreply.github.com>
2023-01-07 21:13:11 +0000

    commit

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -21,6 +21,8 @@ public class DataSource {
 
   }
 
+  // getCustomr method
+
   public static Customer getCustomer(String username) {
     String sql = "select * from customers where username = ?";
     Customer customer = null;
@@ -30,10 +32,10 @@ public class DataSource {
       statement.setString(1, username);
       try (ResultSet resultSet =  statement.executeQuery()) {
         customer = new Customer(
-          resultSet.getInt("ID"),
+          resultSet.getInt("id"),
           resultSet.getString("name"),
           resultSet.getString("username"),
-          resultSet.getString("PASSWORD"),
+          resultSet.getString("password"),
           resultSet.getInt("account_id"));
       }
 
@@ -45,12 +47,39 @@ public class DataSource {
     return customer;
   }
 
+  // getAccount method
+
+  public static Account getAccount(int accountId) {
+    String sql = "select * from accounts where id = ?";
+    Account account = null;
+
+    try(Connection connection = connect(); PreparedStatement statement = connection.prepareStatement(sql)){
+      
+      statement.setInt(1, accountId);
+
+      try(ResultSet resultSet = statement.executeQuery()){
+        account = new Account(
+          resultSet.getInt("ID"),
+          resultSet.getString("TYPE"),
+          resultSet.getDouble("BALANCE"));
+      }
+    } catch(SQLException e){
+        e.printStackTrace();
+        }
+
+      return account;
+  }
   public static void main(String[] args) {
     Customer customer = getCustomer("ojamblinbx@ycombinator.com");
+    Account account = getAccount(customer.getAccountID());
+
     System.out.println(customer.getName());
-    System.out.println(customer.getUsername());
-    System.out.println(customer.getAccountID());
-    System.out.println(customer.getId());
+    System.out.println(account.getBalance());
+    System.out.println(account.getId());
+    System.out.println(account.getType());
+
+    // customer.setPassword("mypassword");
     System.out.println(customer.getPassword());
+    System.out.println(customer.getId());
   }
 }

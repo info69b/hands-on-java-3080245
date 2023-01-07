ac40b0a02e7885a90efb7d083e46357dea503453
info69b <42078775+info69b@users.noreply.github.com>
2023-01-07 20:32:53 +0000

    commit

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,6 +2,8 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -17,5 +19,38 @@ public class DataSource {
     
     return connection;
 
+  }
+
+  public static Customer getCustomer(String username) {
+    String sql = "select * from customers where username = ?";
+    Customer customer = null;
+
+    try(Connection connection = connect(); PreparedStatement statement = connection.prepareStatement(sql)) {
+
+      statement.setString(1, username);
+      try (ResultSet resultSet =  statement.executeQuery()) {
+        customer = new Customer(
+          resultSet.getInt("ID"),
+          resultSet.getString("name"),
+          resultSet.getString("username"),
+          resultSet.getString("PASSWORD"),
+          resultSet.getInt("account_id"));
+      }
+
+
+    }catch(SQLException e) {
+      e.printStackTrace();
+    }
+
+    return customer;
+  }
+
+  public static void main(String[] args) {
+    Customer customer = getCustomer("ojamblinbx@ycombinator.com");
+    System.out.println(customer.getName());
+    System.out.println(customer.getUsername());
+    System.out.println(customer.getAccountID());
+    System.out.println(customer.getId());
+    System.out.println(customer.getPassword());
   }
 }

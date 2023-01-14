1a0bbc2c653fddb8825e0883bb37203372f67bdb
info69b <42078775+info69b@users.noreply.github.com>
2023-01-14 16:02:48 +0000

    commit

---
+++ src/main/java/bank/Menu.java
@@ -0,0 +1,46 @@
+package bank;
+
+import java.util.Scanner;
+import javax.security.auth.login.LoginException;
+
+public class Menu {
+
+  private Scanner scanner;
+
+  public static void main(String[] args) {
+    System.out.println("Welcome to Globe Bank International");
+
+    Menu menu = new Menu();
+
+    menu.scanner = new Scanner(System.in);
+
+    Customer customer = menu.authenticateUser();
+
+    if (customer != null) {
+
+      Account account = DataSource.getAccount(customer.getAccountID());
+
+    }
+
+    menu.scanner.close();
+
+  }
+
+  private Customer authenticateUser() {
+    System.out.println("Please enter yourt user name!");
+    String username = scanner.next();
+
+    System.out.println("Please enter your password");
+    String password = scanner.next();
+
+    Customer customer = null;
+    try {
+      customer = Authenticator.login(username, password);
+    } catch (LoginException e) {
+      System.out.println("There was an error: " + e.getMessage());
+    }
+
+    return customer;
+  }
+
+}
